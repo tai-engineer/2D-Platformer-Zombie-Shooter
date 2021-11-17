94aea686888a68a7f58aa14b11aa623f3ae7f54b
TaiVan <vcmtspkt@gmail.com>
2021-11-17 11:34:36 +0700

    Added Grenade and updated Damager component
    - Added explosion, sound, material, texture from Epic Tool FX
    - Added Grenade component.
    - Added Circle type into Damager component

--- Assets/Scripts/Common/Damager.cs
+++ Assets/Scripts/Common/Damager.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System;
+
 namespace PZS
 {
     public class Damager : MonoBehaviour
     {
+        public enum RaycastType { Rectangular, Circle };
         [Serializable]
         public class DamageableEvent: UnityEvent<Damager, Damageable> { }
+        [Serializable]
+        public class NonDamageableEvent: UnityEvent<Damager> { }
 
 
         public int damage = 1;
+
+        public RaycastType type;
         public Vector2 offset;
         public Vector2 size = new Vector2(1f,1f);
+
+        [Range(0f, 360f)]
+        public float angle = 45f;
+        public float radius = 1f;
+        [HideInInspector]
+        public Vector2 viewDirection = Vector2.left;
+
         public bool canHitTrigger;
         public bool disableDamageAfterHit = true;
         public LayerMask hittableLayer;
         public DamageableEvent onDamageableHit;
+        public NonDamageableEvent onNonDamageableHit;
 
 
         ContactFilter2D _contactFilter;
@@ -30,26 +44,57 @@ namespace PZS
 
         void FixedUpdate()
         {
-
             if (!_canDamage)
                 return;
 
-            Vector2 scale = transform.lossyScale;
-            Vector2 facingOffset = Vector2.Scale(offset, scale);
-            Vector2 scaledSize = Vector2.Scale(scale, size);
-
-            Vector2 pointA = (Vector2)transform.position + facingOffset - scaledSize * 0.5f;
-            Vector2 pointB = pointA + scaledSize;
-
-            int hitCount = Physics2D.OverlapArea(pointA, pointB, _contactFilter, _attackOverlapResults);
-            for (int i = 0; i < hitCount; i++)
+            int hitCount;
+            if (type == RaycastType.Rectangular)
             {
-                if(_attackOverlapResults[i].TryGetComponent<Damageable>(out Damageable damageable))
+                Vector2 scale = transform.lossyScale;
+                Vector2 facingOffset = Vector2.Scale(offset, scale);
+                Vector2 scaledSize = Vector2.Scale(scale, size);
+
+                Vector2 pointA = (Vector2)transform.position + facingOffset - scaledSize * 0.5f;
+                Vector2 pointB = pointA + scaledSize;
+
+                hitCount = Physics2D.OverlapArea(pointA, pointB, _contactFilter, _attackOverlapResults);
+                for (int i = 0; i < hitCount; i++)
                 {
-                    damageable.TakeDamage(this);
-                    onDamageableHit.Invoke(this, damageable);
+                    if (_attackOverlapResults[i].TryGetComponent<Damageable>(out Damageable damageable))
+                    {
+                        damageable.TakeDamage(this);
+                        onDamageableHit.Invoke(this, damageable);
+                    }
+                    else
+                    {
+                        onNonDamageableHit.Invoke(this);
+                    }
+
                     if (disableDamageAfterHit)
                         DisableDamage();
+                }
+            }
+            else if (type == RaycastType.Circle)
+            {
+                hitCount = Physics2D.OverlapCircle(transform.position, radius, _contactFilter, _attackOverlapResults);
+                
+                for (int i = 0; i < hitCount; i++)
+                {
+                    if (_attackOverlapResults[i].TryGetComponent<Damageable>(out Damageable damageable))
+                    {
+                        Vector2 distance = _attackOverlapResults[i].transform.position - transform.position;
+                        float targetAngle = Vector2.SignedAngle(distance, viewDirection);
+                        if (targetAngle < 0)
+                            targetAngle = 360 - targetAngle * -1;
+
+                        if (targetAngle > angle)
+                            continue;
+
+                        damageable.TakeDamage(this);
+                        onDamageableHit.Invoke(this, damageable);
+                        if (disableDamageAfterHit)
+                            DisableDamage();
+                    }
                 }
             }
         }
